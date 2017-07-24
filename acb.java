89d8bfbb4ccea9c67538bbe802af498a58bb2e02
kanikannan19 <kanimozhi08061997@gmail.com>
2017-07-24 11:17:38 +0530

    Add files via upload

---
+++ acb.java
@@ -0,0 +1,16 @@
+package ka;
+import java.util.Scanner;
+public class acb {
+	 public static void main(String[] args) 
+	  {
+		int a,b,c;
+		System.out.println("Enter the first number");
+	    Scanner s=new Scanner(System.in);
+	    a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+	    System.out.println("The subtraction is");
+		c=a-b;
+		System.out.println(c);
+	  }
+}
