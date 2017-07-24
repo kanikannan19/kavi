91de0220551a7f5826a8913935b7a1f39509cb0f
kanikannan19 <kanimozhi08061997@gmail.com>
2017-07-24 11:46:10 +0530

    Add files via upload

---
+++ Aef.java
@@ -0,0 +1,15 @@
+package ka;
+
+public class Aef {
+	    public static void main(String[] args)
+	    {	       int a,b,c=0;
+	        for(b=0;b<50;b++)
+	        {	      for(a=0;a<50;a++)
+	        {	         if(a+b==25 && a-b==13)
+	            {
+	             System.out.println(a+" "+b);
+	             c=a*b; 
+	           }    }    }
+    System.out.println("Product is:"+c);
+	}	}
+
